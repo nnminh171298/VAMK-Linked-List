2dd23b8b360c930b058d2fd24cbdaa3e468df387
nnminh171298 <nnminh171298@gmail.com>
2019-11-17 15:13:28 +0200

    fix wrong changes when adding circular check

--- linked_list_testcase_basic.c
+++ linked_list_testcase_basic.c
@@ -1,44 +1,9 @@
 #include <gtest/gtest.h>
+
 #include <cstdio>
-#include <cstring>
+
+#include "helper.h"
 #include "linked_list.h"
-
-//----------------------Helpers------------------------------------------------
-
-linked_list *generateNode(int index, int data)
-{
-	char *data_ptr = (char *) malloc(sizeof(char) * 20);
-	sprintf(data_ptr, "Data %d", data);
-	
-	linked_list *new_node = (linked_list *) malloc(sizeof(linked_list));
-	new_node->data = data_ptr;
-	new_node->index = index;
-	new_node->next = nullptr;
-	
-	return new_node;
-}
-
-void freeNode(linked_list *node)
-{
-	free(node->data);
-	free(node);
-}
-
-bool checkNode(linked_list *node, int index, char const *data, linked_list *next)
-{
-	bool data_compare = false;
-	if(node->data == nullptr && data == nullptr)
-		data_compare = true;
-	else if(node->data != nullptr && data != nullptr)
-		data_compare = (strcmp(data, node->data) == 0);
-
-	return (node->index == index) && (node->next == next) && data_compare;
-}
-
-void linkNodes(linked_list **prev_node, linked_list **next_node)
-{
-	(*prev_node)->next = *next_node;
-}
 
 //----------------------Add----------------------------------------------------
 
